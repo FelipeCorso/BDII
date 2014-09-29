d1e6cfe709eb3f4d29c88bf2bf72c49667109b6e
leanseefeld <leanseefeld@gmail.com>
2014-09-29 00:31:40 -0300

    Código gerado pelo GALS

--- jsondb-parser/src/br/furb/jsondb/parser/Constants.java
+++ jsondb-parser/src/br/furb/jsondb/parser/Constants.java
@@ -6,48 +6,46 @@ public interface Constants extends ScannerConstants, ParserConstants
     int DOLLAR   = 1;
 
     int t_id = 2;
-    int t_id_ref = 3;
-    int t_numero = 4;
-    int t_literal = 5;
-    int t_data = 6;
-    int t_TOKEN_7 = 7; //";"
-    int t_TOKEN_8 = 8; //","
-    int t_TOKEN_9 = 9; //"."
+    int t_numero = 3;
+    int t_literal = 4;
+    int t_data = 5;
+    int t_TOKEN_6 = 6; //";"
+    int t_TOKEN_7 = 7; //"("
+    int t_TOKEN_8 = 8; //")"
+    int t_TOKEN_9 = 9; //","
     int t_TOKEN_10 = 10; //"*"
-    int t_TOKEN_11 = 11; //"("
-    int t_TOKEN_12 = 12; //")"
-    int t_TOKEN_13 = 13; //"="
+    int t_TOKEN_11 = 11; //"."
+    int t_TOKEN_12 = 12; //"="
+    int t_TOKEN_13 = 13; //">"
     int t_TOKEN_14 = 14; //"<"
-    int t_TOKEN_15 = 15; //"<="
-    int t_TOKEN_16 = 16; //">"
-    int t_TOKEN_17 = 17; //">="
-    int t_TOKEN_18 = 18; //"<>"
-    int t_TOKEN_19 = 19; //"null"
-    int t_SET = 20;
-    int t_DATABASE = 21;
-    int t_CREATE = 22;
-    int t_DROP = 23;
-    int t_DESCRIBE = 24;
-    int t_TABLE = 25;
-    int t_CONSTRAINT = 26;
-    int t_PRIMARY = 27;
-    int t_FOREIGN = 28;
+    int t_TOKEN_15 = 15; //">="
+    int t_TOKEN_16 = 16; //"<="
+    int t_TOKEN_17 = 17; //"<>"
+    int t_CREATE = 18;
+    int t_DATABASE = 19;
+    int t_TABLE = 20;
+    int t_NUMBER = 21;
+    int t_VARCHAR = 22;
+    int t_DATE = 23;
+    int t_CHAR = 24;
+    int t_CONSTRAINT = 25;
+    int t_NULL = 26;
+    int t_NOT = 27;
+    int t_PRIMARY = 28;
     int t_KEY = 29;
     int t_REFERENCES = 30;
-    int t_INSERT = 31;
-    int t_INTO = 32;
-    int t_VALUES = 33;
-    int t_SELECT = 34;
-    int t_FROM = 35;
-    int t_WHERE = 36;
-    int t_AND = 37;
-    int t_OR = 38;
-    int t_NUMBER = 39;
-    int t_VARCHAR = 40;
-    int t_DATE = 41;
-    int t_CHAR = 42;
-    int t_NULL = 43;
-    int t_NOT = 44;
-    int t_INDEX = 45;
+    int t_FOREIGN = 31;
+    int t_INSERT = 32;
+    int t_INTO = 33;
+    int t_VALUES = 34;
+    int t_SELECT = 35;
+    int t_FROM = 36;
+    int t_WHERE = 37;
+    int t_AND = 38;
+    int t_OR = 39;
+    int t_DROP = 40;
+    int t_DESCRIBE = 41;
+    int t_SET = 42;
+    int t_INDEX = 43;
 
 }
