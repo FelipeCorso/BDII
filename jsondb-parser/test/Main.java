2a507424f6664646bc313514be496de9d558b0b3
leanseefeld <leanseefeld@gmail.com>
2014-10-07 13:14:24 -0300

    Rename do pacote das classes geradas pelo GALS

--- jsondb-parser/test/Main.java
+++ jsondb-parser/test/Main.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
-import br.furb.jsondb.parser.Lexico;
-import br.furb.jsondb.parser.Semantico;
-import br.furb.jsondb.parser.Sintatico;
+import br.furb.jsondb.parser.core.Lexico;
+import br.furb.jsondb.parser.core.Semantico;
+import br.furb.jsondb.parser.core.Sintatico;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
 		try (Scanner sc = new Scanner(System.in)) {
-			//			sc.useDelimiter(PROGRAM_DELIMITER_REGEXP);
+			// sc.useDelimiter(PROGRAM_DELIMITER_REGEXP);
 			String program = sc.next();
 			Sintatico sintatico = new Sintatico();
 			Lexico lexico = new Lexico(program);
