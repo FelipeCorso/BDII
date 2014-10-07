ec12b9bfb65041b9cd363adbfec34a388c08ff78
leanseefeld <leanseefeld@gmail.com>
2014-10-07 12:47:33 -0300

    Rename do pacote das classes geradas pelo GALS

--- jsondb-parser/src/br/furb/jsondb/parser/Semantico.java
+++
@@ -1,8 +0,0 @@
-package br.furb.jsondb.parser;
-
-public class Semantico implements Constants {
-
-	public static void executeAction(int action, Token token) {
-		System.out.println("Ação #" + action + ", Token: " + token);
-	}
-}
