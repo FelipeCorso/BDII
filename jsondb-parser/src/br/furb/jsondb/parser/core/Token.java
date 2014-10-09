414dd0803d1cc758e4243548b12c78fb404fb7d9
leanseefeld <leanseefeld@gmail.com>
2014-10-09 12:40:32 -0300

    Exibir lexema junto aos tipos id, numero, literal e data
    Adicionado teste que use o tipo "data"

--- jsondb-parser/src/br/furb/jsondb/parser/core/Token.java
+++ jsondb-parser/src/br/furb/jsondb/parser/core/Token.java
@@ -2,9 +2,9 @@ package br.furb.jsondb.parser.core;
 
 public class Token {
 
-	private static String[] TOKEN_NAMES = { "<vazio>", "fim de programa", "id", "numero", "literal", "data", ";", "(", ")", ",", "*", ".", "=", ">", "<", ">=", "<=", "<>",
-			"CREATE", "DATABASE", "TABLE", "NUMBER", "VARCHAR", "DATE", "CHAR", "CONSTRAINT", "NULL", "NOT", "PRIMARY", "KEY", "REFERENCES", "FOREIGN", "INSERT", "INTO", "VALUES",
-			"SELECT", "FROM", "WHERE", "AND", "OR", "DROP", "DESCRIBE", "SET", "INDEX" };
+	private static String[] TOKEN_NAMES = { "<vazio>", "fim de programa", "identificador", "numero", "literal", "data", ";", "(", ")", ",", "*", ".", "=", ">", "<", ">=", "<=",
+			"<>", "CREATE", "DATABASE", "TABLE", "NUMBER", "VARCHAR", "DATE", "CHAR", "CONSTRAINT", "NULL", "NOT", "PRIMARY", "KEY", "REFERENCES", "FOREIGN", "INSERT", "INTO",
+			"VALUES", "SELECT", "FROM", "WHERE", "AND", "OR", "DROP", "DESCRIBE", "SET", "INDEX" };
 
 	private int id;
 	private String lexeme;
@@ -42,11 +42,14 @@ public class Token {
 
 	@Override
 	public String toString() {
-		return id + " ( " + lexeme + " ) @ " + position;
+		return getClassificacao() + " @ " + position;
 	}
 
 	public String getClassificacao() {
 		if (this.id >= 0 && this.id < TOKEN_NAMES.length) {
+			if (this.id > 1 && this.id < 6) {
+				return TOKEN_NAMES[this.id].concat("(").concat(lexeme).concat(")");
+			}
 			return TOKEN_NAMES[this.id];
 		}
 		return lexeme;
