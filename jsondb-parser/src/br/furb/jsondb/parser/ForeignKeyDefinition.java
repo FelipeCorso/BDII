89fef2ea2abcf21e42d54819d0212c3e9a70c755
leanseefeld <leanseefeld@gmail.com>
2014-10-29 18:28:43 -0200

    Reconhecimento de colunas envolvidas nas restrições

--- jsondb-parser/src/br/furb/jsondb/parser/ForeignKeyDefinition.java
+++ jsondb-parser/src/br/furb/jsondb/parser/ForeignKeyDefinition.java
@@ -8,9 +8,13 @@ public class ForeignKeyDefinition extends KeyDefinition {
 
 	private TableIdentifier targetTable;
 	private List<ColumnIdentifier> targetColumns;
+	
+	public ForeignKeyDefinition(String name) {
+		this(name, null, null);
+	}
 
-	public ForeignKeyDefinition(String name, ConstraintKind kind, ColumnIdentifier sourceColum, ColumnIdentifier targetColumn) {
-		super(name, kind, sourceColum);
+	public ForeignKeyDefinition(String name, ColumnIdentifier sourceColum, ColumnIdentifier targetColumn) {
+		super(name, ConstraintKind.FOREIGN_KEY, sourceColum);
 
 		targetColumn = Objects.requireNonNull(targetColumn, "a target column must be provided for the foreign key definition");
 		if (!targetColumn.getTable().isPresent()) {
@@ -20,6 +24,18 @@ public class ForeignKeyDefinition extends KeyDefinition {
 
 		this.targetColumns = new LinkedList<>();
 		this.targetColumns.add(targetColumn);
+	}
+	
+	public void addTargetColumn(ColumnIdentifier column) {
+		this.targetColumns.add(Objects.requireNonNull(column, "cannot add null as a target column"));
+	}
+	
+	public TableIdentifier getTargetTable() {
+		return targetTable;
+	}
+	
+	public void setTargetTable(TableIdentifier targetTable) {
+		this.targetTable = Objects.requireNonNull(targetTable, "cannot set null as target column");
 	}
 
 }
