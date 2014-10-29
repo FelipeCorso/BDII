095fd67de11591608c57e1c6af8fe97fa60324ba
janainamendonca <janaezico@gmail.com>
2014-10-28 23:35:00 -0200

    Criação de propriedade para configurar o diretório onde vai ficar o jsondb

---
+++ jsondb-store/src/br/furb/jsondb/store/JsonDBProperty.java
@@ -0,0 +1,40 @@
+package br.furb.jsondb.store;
+
+public enum JsonDBProperty {
+
+	JSON_DB_DIR("br.furb.jsondb.dir", "user.home");
+
+	private String property;
+	private String defaultValue;
+
+	JsonDBProperty(String property, String defaultValue) {
+		this.property = property;
+		this.defaultValue = defaultValue;
+	}
+
+	public String getDefaultValue() {
+		return defaultValue;
+	}
+
+	public String getProperty() {
+		return property;
+	}
+
+	public String get() {
+		String value = System.getProperty(property);
+
+		if (value != null) {
+			return value;
+		}
+
+		return defaultValue;
+	}
+	
+	public String set(String value){
+		if(value == null){
+			System.clearProperty(property);
+			return null;
+		}
+		return System.setProperty(property, value);
+	}
+}
