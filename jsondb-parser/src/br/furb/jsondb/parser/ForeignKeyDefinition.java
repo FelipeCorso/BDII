c34203742bae64a6809be1cf3fabc386d28a4c32
leanseefeld <leanseefeld@gmail.com>
2014-10-30 08:24:15 -0200

    [DONE] Reconhecimento de restrições finais

--- jsondb-parser/src/br/furb/jsondb/parser/ForeignKeyDefinition.java
+++ jsondb-parser/src/br/furb/jsondb/parser/ForeignKeyDefinition.java
@@ -8,34 +8,31 @@ public class ForeignKeyDefinition extends KeyDefinition {
 
 	private TableIdentifier targetTable;
 	private List<ColumnIdentifier> targetColumns;
-	
+
 	public ForeignKeyDefinition(String name) {
 		this(name, null, null);
 	}
 
 	public ForeignKeyDefinition(String name, ColumnIdentifier sourceColum, ColumnIdentifier targetColumn) {
 		super(name, ConstraintKind.FOREIGN_KEY, sourceColum);
-
-		targetColumn = Objects.requireNonNull(targetColumn, "a target column must be provided for the foreign key definition");
-		if (!targetColumn.getTable().isPresent()) {
-			throw new NullPointerException("a target table must be provided for the foreign key definition");
-		}
-		this.targetTable = targetColumn.getTable().get();
-
 		this.targetColumns = new LinkedList<>();
-		this.targetColumns.add(targetColumn);
+
+		if (targetColumn != null) {
+			this.targetTable = targetColumn.getTable().get();
+			this.targetColumns.add(targetColumn);
+		}
 	}
-	
+
 	public void addTargetColumn(ColumnIdentifier column) {
 		this.targetColumns.add(Objects.requireNonNull(column, "cannot add null as a target column"));
 	}
-	
+
 	public TableIdentifier getTargetTable() {
 		return targetTable;
 	}
-	
+
 	public void setTargetTable(TableIdentifier targetTable) {
-		this.targetTable = Objects.requireNonNull(targetTable, "cannot set null as target column");
+		this.targetTable = Objects.requireNonNull(targetTable, "cannot set null as target table");
 	}
 
 }
--- jsondb-parser/src/br/furb/jsondb/parser/TableDefinition.java
+++ jsondb-parser/src/br/furb/jsondb/parser/TableDefinition.java
@@ -8,10 +8,12 @@ public class TableDefinition implements IStructure {
 
 	private TableIdentifier tableName;
 	private final List<ColumnDefinition> columns;
+	private final List<ConstraintDefinition> finalConstraints;
 
 	public TableDefinition(TableIdentifier tableName) {
 		this.tableName = Objects.requireNonNull(tableName, "a table identifier must be provided for the table definition");
 		this.columns = new LinkedList<>();
+		this.finalConstraints = new LinkedList<>();
 	}
 
 	@Override
@@ -20,11 +22,15 @@ public class TableDefinition implements IStructure {
 	}
 
 	public final void addColumnDefinition(ColumnDefinition column) {
-		this.columns.add(column);
+		this.columns.add(Objects.requireNonNull(column, "cannot add a null column"));
 	}
 
 	public final TableIdentifier getTableIdentifier() {
 		return tableName;
+	}
+
+	public void addFinalConstraint(ConstraintDefinition constraint) {
+		this.finalConstraints.add(Objects.requireNonNull(constraint, "cannot add a null constraint"));
 	}
 
 }
