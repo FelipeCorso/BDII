1f36576818270a58f2722645e6f36f1299e704ae
leanseefeld <leanseefeld@gmail.com>
2014-11-04 19:48:29 -0200

    Enumeração para buscar os arquivos de testes

--- jsondb-parser/test/br/furb/jsondb/parser/core/SemanticoTest.java
+++ jsondb-parser/test/br/furb/jsondb/parser/core/SemanticoTest.java
@@ -1,35 +1,78 @@
 package br.furb.jsondb.parser.core;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import br.furb.jsondb.parser.IStatement;
+
 public class SemanticoTest {
+
+	private static enum TestFiles {
+
+		TEST_CREATE_DATABASE("testCreateDatabase"), //
+		TEST_CREATE_INDEX("testCreateIndex"), //
+		TEST_CREATE_INDEX_$QUOTED_ID("testCreateIndex_quotedId"), //
+		TEST_CREATE_TABLE("testCreateTable"), //
+		TEST_DESCRIBE_TABLE("testDescribeTable"), //
+		TEST_DROP_INDEX("testDropIndex"), //
+		TEST_DROP_INDEX_$QUOTED_ID("testDropIndex_quotedId"), //
+		TEST_DROP_TABLE("testDropTable"), //
+		TEST_DROP_TABLE_$QUOTED_ID("testDropTable_quotedId"), //
+		TEST_INSERT_INTO_$NO_SOURCE_COLUMNS_$QUOTED_ID("testInsertInto_noSourceColumns_quotedId"), //
+		TEST_SELECT_ALL_FROM_MANY_$NO_WHERE("testSelectAllFromMany_noWhere"), //
+		TEST_SELECT_ALL_FROM_ONE_$ALL_WHERE_OPERATOS("testSelectAllFromOne_allWhereOperatos"), //
+		TEST_SELECT_MANY_QUALIFIED_FROM_MANY("testSelectManyQualifiedFromMany"), //
+		TEST_SET_DATABASE("testSetDatabase"), //
+		TEST_SET_DATABASE_$QUOTED_ID("testSetDatabase_quotedId"); //
+
+		private String fileName;
+
+		TestFiles(String fileName) {
+			this.fileName = fileName;
+		}
+
+		public String getFileName() {
+			return fileName;
+		}
+
+		public File getFile() {
+			return new File(this.fileName);
+		}
+
+		public File getFile(File parentDir) {
+			return new File(parentDir, this.fileName);
+		}
+
+		public String readFile() throws FileNotFoundException, IOException {
+			return readFile(new File("."));
+		}
+
+		public String readFile(File parentDir) throws FileNotFoundException, IOException {
+			StringBuilder content = new StringBuilder();
+			try (FileReader fr = new FileReader(getFile(parentDir)); //
+					BufferedReader bf = new BufferedReader(fr)) {
+				String line = null;
+				while ((line = bf.readLine()) != null) {
+					content.append(line);
+				}
+			}
+			return content.toString();
+		}
+	}
+
+	private static final File TESTS_DIR = SemanticoTest.class.getClassLoader().;
 
 	private Sintatico sintatico;
 	private Lexico lexico;
 	private Semantico semantico;
-
-	private static class TestFiles {
-
-		static final String TEST_CREATE_DATABASE = "testCreateDatabase";
-		static final String TEST_CREATE_INDEX = "testCreateIndex";
-		static final String TEST_CREATE_INDEX$QUOTED_ID = "testCreateIndex_quotedId";
-		static final String TEST_CREATE_TABLE = "testCreateTable";
-		static final String TEST_DESCRIBE_TABLE = "testDescribeTable";
-		static final String TEST_DROP_INDEX = "testDropIndex";
-		static final String TEST_DROP_INDEX$QUOTED_ID = "testDropIndex_quotedId";
-		static final String TEST_DROP_TABLE = "testDropTable";
-		static final String TEST_DROP_TABLE$QUOTED_ID = "testDropTable_quotedId";
-		static final String TEST_INSERT_INTO$NO_SOURCE_COLUMNS$QUOTED_ID = "testInsertInto_noSourceColumns_quotedId";
-		static final String TEST_SELECT_ALL_FROM_MANY$NO_WHERE = "testSelectAllFromMany_noWhere";
-		static final String TEST_SELECT_ALL_FROM_ONE$ALL_WHERE_OPERATOS = "testSelectAllFromOne_allWhereOperatos";
-		static final String TEST_SELECT_MANY_QUALIFIED_FROM_MANY = "testSelectManyQualifiedFromMany";
-		static final String TEST_SET_DATABASE = "testSetDatabase";
-		static final String TEST_SET_DATABASE$QUOTED_ID = "testSetDatabase_quotedId";
-
-	}
 
 	@Before
 	public void setUp() {
@@ -39,14 +82,19 @@ public class SemanticoTest {
 
 	@Test
 	public void testCreateDatabase() throws Exception {
-		parse(TestFiles.TEST_CREATE_DATABASE);
+		IStatement stm = parse(TestFiles.TEST_CREATE_DATABASE);
 
 		fail("Not yet implemented");
 	}
 
-	public void parse(String program) throws LexicalError, SyntaticError {
-		this.lexico = new Lexico(program);
+	public IStatement parse(TestFiles testFile) throws LexicalError, SyntaticError {
+		try {
+			this.lexico = new Lexico(testFile.readFile(TESTS_DIR));
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 		sintatico.parse(lexico, semantico);
+		return semantico.getStatement();
 	}
 
 }
