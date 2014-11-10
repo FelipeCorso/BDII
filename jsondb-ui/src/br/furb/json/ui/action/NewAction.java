113ce4074a8e6e53f38fe708059e21f0355d296a
FelipeCorso <felipe.corso@live.com>
2014-11-09 22:18:32 -0200

    Ajustdo comando de criação de database;

--- jsondb-ui/src/br/furb/json/ui/action/NewAction.java
+++ jsondb-ui/src/br/furb/json/ui/action/NewAction.java
@@ -1,14 +1,44 @@
 package br.furb.json.ui.action;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
+
 import br.furb.json.ui.Principal;
+import br.furb.json.ui.dialog.Dialog;
+import br.furb.json.ui.panel.treeMenu.ManagerTreeMenu;
 import br.furb.json.ui.status.EStatus;
+import br.furb.jsondb.parser.SQLParserException;
+import br.furb.jsondb.store.metadata.DatabaseMetadata;
+import br.furb.jsondb.store.utils.JsonUtils;
 
 public class NewAction {
-	public static void executeAction(Principal principal) {
-		principal.getTabbedPanel().getCommandPanel().getTextEditor().setText("");
-		principal.getTabbedPanel().getCommandPanel().getTextMsg().setText("");
-		principal.getTabbedPanel().getCommandPanel().getLbFilePath().setText("");
-		principal.getKeyListener().setTextoEditor("");
-		principal.getTabbedPanel().getCommandPanel().getLbStatus().setText(EStatus.NAO_MODIFICADO.toString());
+	public static void executeAction(Principal principal, JTree jTree, DefaultMutableTreeNode dataBaseNode) {
+
+		try {
+			String databaseDir = Dialog.getInstance().createDatabaseDir(principal);
+
+			DatabaseMetadata database = JsonUtils.parseJsonToObject(new File(databaseDir), DatabaseMetadata.class);
+
+			if (!principal.getDatabases().containsKey(database.getName())) {
+				principal.addDataBase(database);
+
+				ManagerTreeMenu.createNodesDatabase(dataBaseNode, database);
+
+				((javax.swing.tree.DefaultTreeModel) jTree.getModel()).reload(ManagerTreeMenu.sort(dataBaseNode));
+
+				principal.getTabbedPanel().getCommandPanel().getTextEditor().setText("");
+				principal.getTabbedPanel().getCommandPanel().getTextMsg().setText("");
+				principal.getTabbedPanel().getCommandPanel().getLbFilePath().setText("");
+				principal.getKeyListener().setTextoEditor("");
+				principal.getTabbedPanel().getCommandPanel().getLbStatus().setText(EStatus.NAO_MODIFICADO.toString());
+
+			}
+		} catch (IOException | SQLParserException e) {
+			System.err.println("ERRO FATAL!\nNão foi possível realizar a criação do arquivo!");
+			e.printStackTrace();
+		}
 	}
 }
--- jsondb-ui/src/br/furb/json/ui/action/OpenAction.java
+++ jsondb-ui/src/br/furb/json/ui/action/OpenAction.java
@@ -1,7 +1,5 @@
 package br.furb.json.ui.action;
 
-import java.awt.FileDialog;
-import java.awt.Frame;
 import java.io.File;
 import java.io.IOException;
 
@@ -9,6 +7,7 @@ import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
 
 import br.furb.json.ui.Principal;
+import br.furb.json.ui.dialog.Dialog;
 import br.furb.json.ui.panel.treeMenu.ManagerTreeMenu;
 import br.furb.jsondb.store.metadata.DatabaseMetadata;
 import br.furb.jsondb.store.utils.JsonUtils;
@@ -16,7 +15,7 @@ import br.furb.jsondb.store.utils.JsonUtils;
 public class OpenAction {
 	public static void executeAction(Principal principal, JTree jTree, DefaultMutableTreeNode dataBaseNode) {
 		try {
-			String databaseDir = loadDataBaseDir(principal);
+			String databaseDir = Dialog.getInstance().loadDatabaseDir(principal);
 
 			DatabaseMetadata database = JsonUtils.parseJsonToObject(new File(databaseDir), DatabaseMetadata.class);
 
@@ -31,21 +30,6 @@ public class OpenAction {
 			System.err.println("ERRO FATAL!\nNão foi possível realizar a leitura do arquivo!");
 			e.printStackTrace();
 		}
-	}
-
-	private static String loadDataBaseDir(Frame frame) {
-		String filePath = "";
-
-		FileDialog fileDialog = new FileDialog(frame, "Abrir", FileDialog.LOAD);
-		fileDialog.setDirectory("C:\\");
-		fileDialog.setVisible(true);
-
-		filePath = fileDialog.getDirectory() + fileDialog.getFile();
-
-		if (!filePath.equalsIgnoreCase("C:\\null")) {
-			return filePath;
-		}
-		return "";
 	}
 
 }
