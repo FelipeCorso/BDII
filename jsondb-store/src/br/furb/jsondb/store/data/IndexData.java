14eb972dd804dca740c3feddd78978bcc6572de5
Janaína Mendonça <janaezico@gmail.com>
2014-11-12 00:05:54 -0200

    Alterado para utilizar mapa normal ao invés do MultiKeyMap, estava apresentando problemas de hash quando o mapa era criado a partir da leitura do arquivo json.

--- jsondb-store/src/br/furb/jsondb/store/data/IndexData.java
+++ jsondb-store/src/br/furb/jsondb/store/data/IndexData.java
@@ -2,10 +2,9 @@ package br.furb.jsondb.store.data;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
-
-import org.apache.commons.collections4.keyvalue.MultiKey;
-import org.apache.commons.collections4.map.MultiKeyMap;
+import java.util.Map;
 
 public class IndexData {
 
@@ -13,7 +12,7 @@ public class IndexData {
 
 	private List<String> columns = new ArrayList<String>();
 
-	private MultiKeyMap<String, List<Integer>> mapEntries = new MultiKeyMap<String, List<Integer>>();
+	private Map<List<String>, List<Integer>> mapEntries = new HashMap<List<String>, List<Integer>>();
 
 	public List<String> getColumns() {
 		return columns;
@@ -25,51 +24,23 @@ public class IndexData {
 
 	public void addEntry(int index, String... values) {
 		List<Integer> indexes = null;
-		MultiKey<String> multiKey = new MultiKey<String>(values);
+		List<String> multiKey = Arrays.asList(values);
 		if (mapEntries.containsKey(multiKey)) {
 			indexes = mapEntries.get(multiKey);
 		} else {
 			indexes = new ArrayList<Integer>();
 			mapEntries.put(multiKey, indexes);
 		}
-
 		indexes.add(index);
-
 	}
 
 	public boolean containsEntry(String... values) {
-		return mapEntries.containsKey(new MultiKey<String>(values));
+		return mapEntries.containsKey(Arrays.asList(values));
 	}
 
 	public List<Integer> getEntry(String... values) {
-		return mapEntries.get(new MultiKey<String>(values));
+		return mapEntries.get(Arrays.asList(values));
 	}
-
-	// public static void main(String[] args) {
-	//
-	// IndexData index = new IndexData();
-	//
-	// index.addColumn("NumEmp");
-	// index.addColumn("TipCol");
-	//
-	// index.addEntry(1, "1", "1");
-	// index.addEntry(2, "1", "2");
-	// index.addEntry(3, "2", "3");
-	//
-	// StringWriter writer = new StringWriter();
-	// JsonUtils.parseObjectToJson(writer, index, IndexData.class);
-	// System.out.println(writer.toString());
-	//
-	// index = JsonUtils.parseJsonToObject(new StringReader(writer.toString()),
-	// IndexData.class);
-	//
-	// for (Entry<MultiKey<? extends String>, List<Integer>> entry :
-	// index.mapEntries.entrySet()) {
-	// System.out.println(entry.getKey() + " = " + entry.getValue());
-	//
-	// }
-	//
-	// }
 
 	public String getName() {
 		return name;
@@ -79,15 +50,4 @@ public class IndexData {
 		this.name = name;
 	}
 
-	public static void main(String[] args) {
-		MultiKeyMap<String, List<Integer>> map = new MultiKeyMap<String, List<Integer>>();
-
-		map.put("100", "200", Arrays.asList(1));
-		map.put("200", "100", Arrays.asList(2));
-
-		System.out.println(map.size());
-
-		System.out.println(map.get("100", "200"));
-		System.out.println(map.get("200", "100"));
-	}
 }
